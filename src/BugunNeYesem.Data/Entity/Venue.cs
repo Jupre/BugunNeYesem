7a0070802919d2c9c6fd1eb8be23b6e17230dece
Mehmet Sabancıoğlu <mehmet.sabancioglu@hotmail.com>
2015-05-02 13:52:19 +0300

    Venue Entity works

--- src/BugunNeYesem.Data/Entity/Venue.cs
+++ src/BugunNeYesem.Data/Entity/Venue.cs
@@ -1,8 +1,19 @@
-﻿namespace BugunNeYesem.Data.Entity
+﻿using System;
+
+namespace BugunNeYesem.Data.Entity
 {
     public class Venue : BaseEntity
     {
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string Contact { get; set; }
+        public string Location { get; set; }
+        public string Url { get; set; }
+        public string Price { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Likes { get; set; }
+        public string Attributes { get; set; }
+
     }
 }
