6697ad781f4b3cf45552ef19cd1e9e77482196db
Bilal Korkmaz <bllkrkmz@gmail.com>
2015-05-02 14:01:15 +0200

    value service operations

---
+++ src/BugunNeYesem.Test/VenueServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+using BugunNeYesem.Logic;
+using BugunNeYesem.Logic.VenueServices;
+using NUnit.Framework;
+
+namespace BugunNeYesem.Test
+{
+    [TestFixture]
+    public class VenueServiceTests
+    {
+        [Test]
+        public void get_venues()
+        {
+            var locationService = new LocationService();
+            var location = locationService.GetLocation();
+
+            var service = new VenueService();
+
+            var venues = service.GetVenues(location);
+
+            Assert.IsNotEmpty(venues);
+        }
+    }
+}
