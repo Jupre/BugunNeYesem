6c9d2463164417a838d4073ea7b44f15a62ba573
Mehmet Sabancıoğlu <mehmet.sabancioglu@hotmail.com>
2015-05-02 14:02:34 +0300

    database tables add

---
+++ src/BugunNeYesem.Database/dbo/Tables/Recommendation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Recommendation]
+(
+	[Id]				INT             IDENTITY (1, 1) NOT NULL, 
+    [CreatedDate]		DATETIME						NULL, 
+	[VenueId]			INT								NULL, 
+    [VenueName]			NVARCHAR(150)					NULL, 
+)
---
+++ src/BugunNeYesem.Database/dbo/Tables/Venue.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Venue]
+(
+	[Id]				INT             IDENTITY (1, 1) NOT NULL, 
+    [CreatedDate]		DATETIME						NULL, 
+    [Name]				NVARCHAR(150)					NULL, 
+    [Contact]			NVARCHAR(150)					NULL, 
+    [Location]			NVARCHAR(1000)					NULL, 
+    [Url]				NVARCHAR(150)					NULL, 
+    [Price]				DECIMAL(18, 2)					NULL, 
+    [Rating]			DECIMAL(18, 2)					NULL, 
+    [Description]		NVARCHAR(1000)					NULL, 
+    [CreatedAt]			DATETIME						NULL, 
+    [Likes]				INT								NULL, 
+    [Attributes]		NVARCHAR(500)					NULL,
+)
